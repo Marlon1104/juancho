bd2ae8d5bc7075e6003bd7c6bc3a6411b1543dca
JADPA-23 <JADPA-23@JADPA-23>
2022-09-24 14:12:35 -0600

    Cambios

---
+++ juan/src/juancho/soul.java
@@ -0,0 +1,13 @@
+package juancho;
+
+public class soul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.printf("lener %nme la pela");
+		
+
+	}
+
+}
